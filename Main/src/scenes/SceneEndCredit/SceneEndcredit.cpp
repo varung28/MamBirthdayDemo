41172556e1422664f912c64edf2bc1d670e14b8a
darshansjogi1008 <darshansjogi1008@gmail.com>
2025-08-20 00:44:31 +0530

    commit for adding birthday slides and end credit slides

--- Main/src/scenes/SceneEndCredit/SceneEndcredit.cpp
+++ Main/src/scenes/SceneEndCredit/SceneEndcredit.cpp
@@ -8,12 +8,20 @@ SceneEndCredit::SceneEndCredit(/* args */)
 
     texQuadEndCreditSlide_2 = new TexturedQuad();
     texQuadEndCreditSlide_2->initialize("resources\\textures\\EndCreditSlide_2.png");
+    
+    texQuadBirthdaySlide_1 = new TexturedQuad();
+    texQuadBirthdaySlide_1->initialize("resources\\textures\\birthday_slide.png");
 
     sdkCreateTimer(&timer);
 }
 
 SceneEndCredit::~SceneEndCredit()
 {
+    if (texQuadBirthdaySlide_1)
+    {
+        delete texQuadBirthdaySlide_1;
+        texQuadBirthdaySlide_1 = nullptr;
+    }
     if (texQuadEndCreditSlide_2)
     {
         delete texQuadEndCreditSlide_2;
@@ -31,6 +39,7 @@ void SceneEndCredit::initialCommandBuffer(VkCommandBuffer &commandBuffer)
 {
     texQuadEndCreditSlide_1->buildCommandBuffers(commandBuffer);
     texQuadEndCreditSlide_2->buildCommandBuffers(commandBuffer);
+    texQuadBirthdaySlide_1->buildCommandBuffers(commandBuffer);
 }
 
 void SceneEndCredit::update(void)
@@ -45,32 +54,44 @@ void SceneEndCredit::update(void)
         completed = true;
     }
 
+    const float yAxis_start_point = -12.0f;
     static float xPos = 0.0f;
-    static float yPos = -6.5f;
+    static float yPos = yAxis_start_point;
 
 
-    if (yPos < 5.0f)
+    if (yPos < -yAxis_start_point)
         yPos += 0.00005f;
     
-    if (texQuadEndCreditSlide_1)
+    if (texQuadBirthdaySlide_1 && yPos < 5)
+    {
+        memset((void*)&mvp_UniformData, 0, sizeof(MVP_UniformData));
+        mvp_UniformData.modelMatrix = glm::mat4(1.0f);
+        mvp_UniformData.modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(xPos, yPos+8.0f, -5.0f)) * glm::scale(glm::mat4(1.0), glm::vec3(3.5f, 1.75f, 1.0f));
+        mvp_UniformData.viewMatrix = glm::mat4(1.0f);
+
+        texQuadBirthdaySlide_1->update(mvp_UniformData);
+    }
+
+    if (texQuadEndCreditSlide_1 && yPos < 8)
     {   
         memset((void*)&mvp_UniformData, 0, sizeof(MVP_UniformData));
         mvp_UniformData.modelMatrix = glm::mat4(1.0f);
-        mvp_UniformData.modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(xPos, yPos + 3.0f, -5.0f)) * glm::scale(glm::mat4(1.0), glm::vec3(3.0f, 1.5f, 1.0f));
+        mvp_UniformData.modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(xPos, yPos + 4.0f, -5.0f)) * glm::scale(glm::mat4(1.0), glm::vec3(3.5f, 1.75f, 1.0f));
         mvp_UniformData.viewMatrix = glm::mat4(1.0f);
 
         texQuadEndCreditSlide_1->update(mvp_UniformData);
     }
 
-    if (texQuadEndCreditSlide_2)
+    if (texQuadEndCreditSlide_2 && yPos < 10)
     {
         memset((void*)&mvp_UniformData, 0, sizeof(MVP_UniformData));
         mvp_UniformData.modelMatrix = glm::mat4(1.0f);
-        mvp_UniformData.modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(xPos, yPos, -5.0f)) * glm::scale(glm::mat4(1.0), glm::vec3(3.0f, 1.5f, 1.0f));
+        mvp_UniformData.modelMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(xPos, yPos, -5.0f)) * glm::scale(glm::mat4(1.0), glm::vec3(3.5f, 1.75f, 1.0f));
         mvp_UniformData.viewMatrix = glm::mat4(1.0f);
 
         texQuadEndCreditSlide_2->update(mvp_UniformData);
     }
+ 
 }
 
 void SceneEndCredit::onResize(int width, int height)
@@ -78,4 +99,6 @@ void SceneEndCredit::onResize(int width, int height)
     texQuadEndCreditSlide_1->resize(width, height);
 
     texQuadEndCreditSlide_2->resize(width, height);
+
+    texQuadBirthdaySlide_1->resize(width, height);
 }
